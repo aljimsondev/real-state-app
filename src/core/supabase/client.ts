dc2bd289178c616246d0b2a51e88d1c79cd6e084
aljimsondev <aljimson.megrino@gmail.com>
2025-10-20 20:55:14 +0800

    Chore | updated environment variable reference

--- src/core/supabase/client.ts
+++ src/core/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr';
-
+console.log(process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!);
 export const createClient = () =>
   createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
   );
--- src/core/supabase/server.ts
+++ src/core/supabase/server.ts
@@ -12,7 +12,7 @@ export async function createClient() {
   const cookieStore = await cookies();
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
