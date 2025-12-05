cd07ddfff8980f23b2d61f549dc8f875e460b2fe
aljimsondev <aljimson.megrino@gmail.com>
2025-12-05 20:50:54 +0800

    Chore | added signin form component

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,5 +1,11 @@
+import SigninForm from '@/components/forms/sign-in.form';
+
 function SigninPage() {
-  return <div>SigninPage</div>;
+  return (
+    <div className="container mx-auto h-[100dvh] justify-center items-center relative flex">
+      <SigninForm />
+    </div>
+  );
 }
 
 export default SigninPage;
